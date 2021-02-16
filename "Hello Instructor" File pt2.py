de0e449476afcc1dd1ad7e142b0926c85aba4041
liamsalmon <79120358+liamsalmon@users.noreply.github.com>
2021-02-15 21:17:37 -0500

    Add files via upload

---
+++ "Hello Instructor" File pt2.py
@@ -0,0 +1,9 @@
+# Introduction to Programming
+# Author: Liam Salmon
+# Date: 15 February 2021
+
+def main():
+    print("Hello, Instructor!")
+    print("Good-bye and Good-night")
+
+main()
---
+++ "Hello Instructor" File.py
@@ -0,0 +1,8 @@
+# Introduction to Programming
+# Author: Liam Salmon
+# Date: 15 February 2021
+
+def main():
+    print("Hello, Instructor!")
+
+main()
